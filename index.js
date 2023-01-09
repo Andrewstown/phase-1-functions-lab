5ebbd3e04e1e5b92720acebac2ed9b27f9376623
+ <stingeyandrewstown@live.com>
2023-01-09 11:06:44 -0600

    commit tree 14d5872f4449ea202927f5bead3f450f6799b81f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let hq = 42
+
+function distanceFromHqInBlocks(dist){
+    return Math.abs(dist - hq)
+}
+
+function distanceFromHqInFeet(dist){
+    return distanceFromHqInBlocks(dist) * 264
+}
+
+function distanceTravelledInFeet(dist1, dist2){
+    return distanceFromHqInFeet(dist2) - distanceFromHqInFeet(dist1)
+}
+
+function calculatesFarePrice(dist1, dist2){
+    let dist = distanceTravelledInFeet(dist1, dist2)
+    if (dist < 400){
+        return 0
+    }else if (dist < 2000){
+        return (dist - 400) / 50
+    }else if (dist < 2500){
+        return 25
+    }else{
+        return 'cannot travel that far'
+    }
+}
